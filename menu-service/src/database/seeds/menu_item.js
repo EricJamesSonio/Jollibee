2890a6a291bd7207c947e76c331c38b5f6d96680
EricJamesSonio <ericjamessonio7@gmail.com>
2026-01-02 10:55:33 +0800

    Added more menu items

--- menu-service/src/database/seeds/menu_item.js
+++ menu-service/src/database/seeds/menu_item.js
@@ -1,32 +1,141 @@
 module.exports = [
+  // 🍗 CHICKEN
   {
-    name: "Chickenjoy C1",
+    name: "Chickenjoy (1-pc)",
     price: 99,
     category_code: "C",
     image_url: "/images/chickenjoy-c1.png"
   },
   {
-    name: "Chickenjoy C2",
+    name: "Chickenjoy (2-pc)",
     price: 149,
     category_code: "C",
     image_url: "/images/chickenjoy-c2.png"
   },
   {
-    name: "Yumburger B1",
+    name: "Chickenjoy with Spaghetti",
+    price: 159,
+    category_code: "C",
+    image_url: "/images/chickenjoy-spaghetti.png"
+  },
+  {
+    name: "Chickenjoy with Palabok",
+    price: 169,
+    category_code: "C",
+    image_url: "/images/chickenjoy-palabok.png"
+  },
+  {
+    name: "Chickenjoy Bucket (6-pc)",
+    price: 549,
+    category_code: "C",
+    image_url: "/images/chickenjoy-bucket-6.png"
+  },
+  {
+    name: "Chickenjoy Bucket (8-pc)",
+    price: 699,
+    category_code: "C",
+    image_url: "/images/chickenjoy-bucket-8.png"
+  },
+  {
+    name: "Chickenjoy Bucket (10-pc)",
+    price: 849,
+    category_code: "C",
+    image_url: "/images/chickenjoy-bucket-10.png"
+  },
+
+  // 🍔 BURGERS
+  {
+    name: "Yumburger",
     price: 59,
     category_code: "B",
     image_url: "/images/yumburger.png"
   },
   {
-    name: "Coke D1",
+    name: "Cheesy Yumburger",
+    price: 69,
+    category_code: "B",
+    image_url: "/images/cheesy-yumburger.png"
+  },
+  {
+    name: "Bacon Cheesy Yumburger",
+    price: 79,
+    category_code: "B",
+    image_url: "/images/bacon-cheesy-yumburger.png"
+  },
+  {
+    name: "Champ",
+    price: 179,
+    category_code: "B",
+    image_url: "/images/champ.png"
+  },
+  {
+    name: "Aloha Champ",
+    price: 199,
+    category_code: "B",
+    image_url: "/images/aloha-champ.png"
+  },
+
+  // 🍨 DESSERTS
+  {
+    name: "Peach Mango Pie",
+    price: 45,
+    category_code: "S",
+    image_url: "/images/peach-mango-pie.png"
+  },
+  {
+    name: "Chocolate Sundae",
+    price: 45,
+    category_code: "S",
+    image_url: "/images/chocolate-sundae.png"
+  },
+  {
+    name: "Caramel Sundae",
+    price: 45,
+    category_code: "S",
+    image_url: "/images/caramel-sundae.png"
+  },
+  {
+    name: "Cookies & Cream Sundae",
+    price: 49,
+    category_code: "S",
+    image_url: "/images/cookies-cream-sundae.png"
+  },
+  {
+    name: "Ube Cheese Pie",
+    price: 49,
+    category_code: "S",
+    image_url: "/images/ube-cheese-pie.png"
+  },
+
+  // 🥤 DRINKS
+  {
+    name: "Coke",
     price: 39,
     category_code: "D",
     image_url: "/images/coke.png"
   },
   {
-    name: "Sundae S1",
+    name: "Coke Zero",
+    price: 39,
+    category_code: "D",
+    image_url: "/images/coke-zero.png"
+  },
+  {
+    name: "Sprite",
+    price: 39,
+    category_code: "D",
+    image_url: "/images/sprite.png"
+  },
+  {
+    name: "Iced Tea",
     price: 45,
-    category_code: "S",
-    image_url: "/images/sundae.png"
+    category_code: "D",
+    image_url: "/images/iced-tea.png"
+  },
+  {
+    name: "Pineapple Juice",
+    price: 49,
+    category_code: "D",
+    image_url: "/images/pineapple-juice.png"
   }
 ];
